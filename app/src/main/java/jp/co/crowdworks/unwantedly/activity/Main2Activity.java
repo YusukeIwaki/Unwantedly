b2b7e502fe94e0e2d525e69f5ee91140ced9d963
Yusuke Iwaki <yusuke.iwaki@crowdworks.co.jp>
2015-04-08 19:15:25 +0900

    SlidingPaneLayoutつかってみた。が、激しく微妙。

--- app/src/main/java/jp/co/crowdworks/unwantedly/activity/Main2Activity.java
+++ app/src/main/java/jp/co/crowdworks/unwantedly/activity/Main2Activity.java
@@ -2,8 +2,13 @@ package jp.co.crowdworks.unwantedly.activity;
 
 import android.os.Bundle;
 import android.support.v4.view.ViewPager;
+import android.support.v4.widget.SlidingPaneLayout;
 import android.support.v7.app.ActionBarActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 import jp.co.crowdworks.unwantedly.R;
 import jp.co.crowdworks.unwantedly.view.SlidingImageTabLayout;
@@ -26,5 +31,22 @@ public class Main2Activity extends ActionBarActivity{
 
         Toolbar toolbar = (Toolbar) findViewById(R.id.activity_main2_toolbar);
 
+
+        initializePane();
+    }
+
+    private void initializePane(){
+        final SlidingPaneLayout pane = (SlidingPaneLayout) findViewById(R.id.activity_main2_sliding_pane);
+
+        ListView menuList = (ListView) findViewById(R.id.activity_main2_slide_menu);
+        menuList.setAdapter(new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, new String[] {"hoge", "fuga", "piyo"}));
+        menuList.setOnItemClickListener(new ListView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                if(pane.isSlideable() && pane.isOpen()) {
+                    pane.closePane();
+                }
+            }
+        });
     }
 }
